0adac3ef689a3fe14cdd89474af0fa2fcb469525
Michael Wiltfong <76107997+mdwiltfong@users.noreply.github.com>
2021-08-12 04:27:08 -0400

    changes to test file

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,12 +1,16 @@
 describe(`Calculating monthly rate`,()=>{
- it(`getCurrentUIValues`,()=>{
-   expect(getCurrentUIValues.toBeInstanceOf(Object));
-
- })
+  const values={
+    amount:5000,
+    years:10,
+    rate:123
+  }
+  it("calculateMonthlyPayment function",()=>{
+    expect(calculateMonthlyPayment(values)).toBe('615000.00')
+  })
   
   
   it("should return a result with 2 decimal places", function() {
-    // ..
+    expect(calculateMonthlyPayment(values)).toBe('615000.00')
   });
 
 
