52c94fb7e6c97e3e50ff032ac96e26357d4d3e47
Michael Wiltfong <76107997+mdwiltfong@users.noreply.github.com>
2021-08-12 03:46:56 -0400

    Created front-end js

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,19 @@
+describe(`Calculating monthly rate`,()=>{
+ it(`getCurrentUIValues`,()=>{
+   expect(getCurrentUIValues.toBeInstanceOf(Object));
 
-it('should calculate the monthly rate correctly', function () {
-  // ...
-});
+ })
+  
+  
+  it("should return a result with 2 decimal places", function() {
+    // ..
+  });
 
 
-it("should return a result with 2 decimal places", function() {
-  // ..
-});
+
+
+
+
+})
 
 /// etc
--- calculator/calculator.js
+++ calculator/calculator.js
@@ -10,6 +10,7 @@ window.addEventListener('DOMContentLoaded', function() {
 });
 
 function getCurrentUIValues() {
+  
   return {
     amount: +(document.getElementById("loan-amount").value),
     years: +(document.getElementById("loan-years").value),
@@ -22,31 +23,33 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
-  let loan_amount=document.getElementById(`loan-amount`);
-  let loan_years=document.getElementById(`loan-years`);
-  let loan_rate=document.getElementById(`loan-rate`);
-  loan_amount.setAttribute(`placeholder`,`$5000`);
-  loan_years.setAttribute(`placeholder`,`10`);
-  loan_rate.setAttribute(`placeholder`,`123`)
+  let p=document.getElementById(`loan-amount`);
+  let n=document.getElementById(`loan-years`);
+  let i=document.getElementById(`loan-rate`);
+  p.value= 5000;
+  n.value=10;
+  i.value=123;
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
-  let p=loan_amount.value;
-  let i=loan_rate.value/12;
-  let n=loan_years.value;
-
-  return [p,i,n]
+ let {amount,years,rate}=getCurrentUIValues();
+ console.log(amount,years,rate)
+ let output=calculateMonthlyPayment({amount,years,rate});
+ console.log(output);
+ updateMonthly(output)
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
-let {p,i,n}=values;
-let x= (1+i)^(-n);
-let mp = (p*i)/(1-x);
+let {amount,years,rate}=values;
+console.log(amount,years,rate)
+let x= Math.pow((1+rate),-years);
+let mp = (amount*rate)/(1-x);
+console.log(x);
 return `${mp.toFixed(2)}`
 }
 
